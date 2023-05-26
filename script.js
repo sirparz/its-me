3b7af4226c64ea7721355153c4490b6a02b2b7bc
Faris <ferishahab11@gmail.com>
2023-05-26 17:41:08 +0700

    Update Header

---
+++ script.js
@@ -0,0 +1,7 @@
+/**
+  Pixelated theme
+  Contain brief about, projects, skills, contact
+  Featured Projects: Al Waha, Armor Trim, Colors
+  Skills: HTML, CSS, Git, Github, Javascript, Node, More...
+  Reach out on: Github, Discord
+*/
