6153acccc9d351abd5f679c827457940bdf3c1ce
Faris <ferishahab11@gmail.com>
2023-05-27 15:02:51 +0700

    Add Project Cards

--- script.js
+++ script.js
@@ -5,3 +5,46 @@
   Skills: HTML, CSS, Git, Github, Javascript, Node, More...
   Reach out on: Github, Discord
 */
+
+function comingSoon() {
+  alert('Repository coming soon.');
+}
+
+const projectCards = document.getElementsByClassName("project");
+for (let i=0; i<projectCards.length; i++) {
+  projectCards[i].style.cursor = "pointer";
+}
+
+
+function gitHubLink(gitHubLink = '') {
+  if (gitHubLink === '') {
+    comingSoon();
+  }
+  else {
+    location.href = gitHubLink;
+  }
+}
+
+const colorRNG = () => {
+  const colors = ['#95C7DB','#DBCCAB','#E2A5D0'];
+  let order = colors[Math.floor(Math.random() * colors.length)];
+  return order; // your colour change
+};
+
+
+for (let i=0; i<projectCards.length; i++) {
+  projectCards[i].addEventListener("mouseover", event => {
+    event.target.style.backgroundColor = colorRNG();
+  },
+  true)
+
+  projectCards[i].addEventListener("mouseout", event => {
+    event.target.style.backgroundColor = 'rgb(241, 241, 241)';
+    
+  },
+  true)
+  
+  //onmouseover = projectCards[i].classList.add(colorRNG());
+}
+// = this.onmouseover = colorRNG;
+console.log(projectCards)
